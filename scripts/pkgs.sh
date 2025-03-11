784d1ec60baff974638d93f944409ff38b79c438
EMO-X <yahyaechcharqui@proton.me>
2025-03-11 15:18:03 +0000

    pkgs installer

---
+++ scripts/pkgs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is for install important pkgs
+
+# run script as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root." 
+   exit 1
+fi
+
+pacman -Syu --noconfirm
+pacman -S --noconfirm fzf neovim zsh starship   
